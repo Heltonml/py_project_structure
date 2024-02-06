67c0b71252b6be7d6065cd7d8db4fd3eabdc2cd7
Helton M Leite <heltonml@gmail.com>
2024-02-06 15:51:15 -0300

    config: adding Flask

--- main_raw.py
+++ main_raw.py
@@ -1,2 +1,10 @@
-def my_function():
-    print('Py project structure...')
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def get_home():
+    return jsonify({ "msg": "Server is running!!!" })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=None)
